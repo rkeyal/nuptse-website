43afbb7bc1c1eb8fe45e96cd7d949ec259767513
Robert Quitt <robertquitt@gmail.com>
2015-07-19 15:53:22 -0700

    Panel fixed
    relative links

--- admin/panel.php
+++ admin/panel.php
@@ -6,28 +6,17 @@
   <body>
     <?php include("../navbar/navbar.php"); ?>
     <div class="container">
-<<<<<<< HEAD
     <a href="/admin/manageusers.php">Manage Users/Groups</a><br />
     <a href="/admin/viewpsets.php">View Problem Sets</a><br />
     <a href="/admin/addpset.php">Add Problem Set</a>
     <?php if (!empty($_GET['groups']) and $_GET['groups'] === "true") {
-=======
-    <a href="./manageusers.php">Manage Users/Groups</a><br />
-    <a href="./viewpsets.php">View Problem Sets</a><br />
-    <a href="./addpset.php">Add Problem Set</a>
-    <?php if (isset($_GET['groups']) and $_GET['groups'] === "true") {
->>>>>>> origin/master
         echo '
             <div class="alert alert-success">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
                 <strong>Success!</strong> Groups modified successfully.
             </div>';
     }
-<<<<<<< HEAD
     if (!empty($_GET['groups']) and $_GET['changed'] === "true") {
-=======
-    if (isset($_GET['changed']) and $_GET['changed'] === "true") {
->>>>>>> origin/master
         echo '
             <div class="alert alert-success">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
