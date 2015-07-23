cd2458d60cca12fc60740edd6f25056cc22d8bf8
rkeyal <rahulkeyal@gmail.com>
2015-07-22 22:06:43 -0700

    Enrollment Fixes

--- classes/registration.php
+++ classes/registration.php
@@ -5,8 +5,8 @@
     <?php include("../navbar/navbar.php"); ?>
     <div class="container" >
     <h1>Enrollment Form</h1>
-	<object data="EnrollmentFormFinal.pdf" type="application/pdf" width="100%" height="800px">
-                       <p>Looks like you can't view <a href="EnrollmentFormFinal.pdf">this file.</a></p>
+	<object data="EnrollmentFormRevA1.pdf" type="application/pdf" width="100%" height="600px">
+                       <p>Looks like you can't view <a href="EnrollmentFormRevA1.pdf">this file.</a></p>
                     </object>
       <?php include("../footer.php"); ?>
 
