1a4d75d2eb51eb89660b6c3baf31f755c48ac842
cdr135 <cdr300947@gmail.com>
2015-07-26 14:54:42 -0700

    Remove github's markers

--- about/foundationoverview.php
+++ about/foundationoverview.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<<<<<<< HEAD
 	<?php include ("../header.php"); ?>
 	<body>
 		<?php include("../navbar/navbar.php"); ?>
@@ -17,23 +16,4 @@
 		<script src="../assets/bootstrap/js/jquery.js"></script>
 		<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 	</body>
-=======
-  <?php include ("../header.php"); ?>
-  <body>
-    <?php include("../navbar/navbar.php"); ?>
-    <div class="container" >
-		<h1>Foundation Overview</h1>
-		<object data="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf" type="application/pdf" width="100%" height="800px">
-			<p>Looks like you can't view <a href="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf">this file.</a></p>
-		</object>
-		<?php include("../footer.php"); ?>
-    </div> <!-- /container -->
-	</div>
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../assets/bootstrap/js/jquery.js"></script>
-    <script src="../assets/bootstrap/js/bootstrap.min.js"></script>
-  </body>
->>>>>>> 324daecc5e07901f64dba0acdfb042e17b83fa90
 </html>
