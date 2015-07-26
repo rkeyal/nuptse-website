95fdbd83c00de18e44e2633a1aea15442022982c
cdr135 <cdr300947@gmail.com>
2015-07-26 14:43:24 -0700

    Fixed nav bar
    and cleaned up code

--- about/foundationoverview.php
+++ about/foundationoverview.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-  <?php include ("../header.php"); ?>
-  <body>
-    <?php include("../navbar/navbar.php"); ?>
-    <div class="container" >
-    <h1>Foundation Overview</h1>
-	<object data="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf" type="application/pdf" width="100%" height="800px">
-                       <p>Looks like you can't view <a href="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf">this file.</a></p>
-                    </object>
-      <?php include("../footer.php"); ?>
-
-    </div> <!-- /container -->
-    </div>
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./assets/bootstrap/js/jquery.js"></script>
-    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
-  </body>
+	<?php include ("../header.php"); ?>
+	<body>
+		<?php include("../navbar/navbar.php"); ?>
+		<div class="container" >
+			<h1>Foundation Overview</h1>
+			<object data="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf" type="application/pdf" width="100%" height="800px">
+				<p>Looks like you can't view <a href="Nuptse%20Foundation%20Overview%20Rev%20D3.pdf">this file.</a></p>
+			</object>
+			<?php include("../footer.php"); ?>
+		</div> <!-- /container -->	
+		<!-- Le javascript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="../assets/bootstrap/js/jquery.js"></script>
+		<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
+	</body>
 </html>
