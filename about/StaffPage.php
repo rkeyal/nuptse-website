7bc1782eef103e351a8be03a931f0b4261b5cc6b
Robert Quitt <robertquitt@gmail.com>
2015-07-26 15:34:43 -0700

    Added Staff

--- about/StaffPage.php
+++ about/StaffPage.php
@@ -4,17 +4,23 @@
 	<body>
 		<?php include("../navbar/navbar.php"); ?>
 		<div class="container" >
-			<h1>Staff</h1>
+			<h1>Nuptse Student Staff</h1>
 			<br />
 			<!-- Number 1=============================================================== -->
-			<p><img src="images/Image_1.png" width="100" height="100" alt="" border="0">TEXTNAME</p>
-			<p>TEXTPAR</p>
+			<h3><img src="../images/staff/shivank_nayak.jpg" width="100" height="100" alt="" border="0" />Shivank Nayak, Vice President of Recruitment</h3>
+			<p>Shivank Nayak is a junior at Westview High School. He has been coaching middle and elementary schoolers in math and science since his freshman year. He enjoys participating in academic competitions like Quizbowl. Shivank also does Speech and Debate to develop his public speaking skills. He has also worked with elementary schoolers looking to improve their writing skills. In his free time, Shivank reads and writes. He is interested in theatre and in discussing current social issues.</p>
 			<hr />
 			<!-- Number 2============================================================== -->
-			<p><img src="images/Image_2.png" width="100" height="100" alt="" border="0">TEXTNAME2</p>
-			<p>TEXTPAR</p>
-			
+			<h3><img src="../images/staff/jefferson_chen.jpg" width="100" height="100" alt="" border="0" />Jefferson Chen, Vice President of Safety and Compliance</h3>
+			<p>Jefferson Chen is a junior at Westview High School. He aims to pursue his passion for STEM with a career in organic chemistry. Jefferson has cultivated a commitment to learning that can be observed through his participation in Westview's Mock Trial team, Chemistry Olympiad, and Harvard's CS50 Computer Science course. Outside of his academic pursuits, Jefferson does volunteer work with Catalyst for Success, Nuptse, and Feeding America. His goal, ultimately, is to foster STEM awareness.</p>
 			<br />
+			
+			<!--
+			<h3><img src="../images/IMAGE_URL" width="100" height="100" alt="" border="10" />NAME AND TITLE</h3>
+			<p>INFO</p>
+			<br />
+			-->
+			
 			<?php include("../footer.php"); ?>
 		</div> <!-- /container -->
 		<!-- Le javascript
