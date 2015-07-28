332f4973f573553de13fb37b8abfb40d69cf2204
LaneiKC126 <kathrynlchan@gmail.com>
2015-07-28 01:25:09 -0700

    Changed arrow color/size
    don't procrastinate

--- index.php
+++ index.php
@@ -73,7 +73,7 @@
 			panelclass: 'panel', //class of panel DIVs each holding content
 			autostep: {enable:true, moveby:1, pause:3000},
 			panelbehavior: {speed:500, wraparound:true, wrapbehavior:'slide', persist:true},
-			defaultbuttons: {enable: true, moveby: 1, leftnav: ['/images/arrow_left.png', 20, 240], rightnav: ['/images/arrow_right.png', -85, 240]},
+			defaultbuttons: {enable: true, moveby: 1, leftnav: ['/images/arrow_left.png', 20, 240], rightnav: ['/images/arrow_right.png', -60, 240]},
 			statusvars: ['statusA', 'statusB', 'statusC'], //register 3 variables that contain current panel (start), current panel (last), and total panels
 			contenttype: ['inline'] //content setting ['inline'] or ['ajax', 'path_to_external_file']
 		})
