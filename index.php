188262c33e9abcd30fe11273d56a909617a2763c
anonymousJasonGanton <anonymous.jasonganton@gmail.com>
2015-08-02 14:57:20 -0700

    RSS

--- index.php
+++ index.php
@@ -153,8 +153,66 @@
       <!-- Example row of columns -->
       <div class="row">
         <div class="span5">
-          <h2>Announcements</h2>
-           <p>Coming Soon.</p>
+          <div id="mainContent">
+      <div class="row">
+	<div class="small-12 columns">
+	<div class="hero-unit">
+	
+	  <h2>Announcements</h2>
+	  <ul>
+	  <?php
+	     date_default_timezone_set("America/Los_Angeles");
+	     require_once($_SERVER['DOCUMENT_ROOT'] . "/lib/simplepie/autoloader.php");
+
+	     $feed = new SimplePie();
+		 $feed->enable_cache(false);
+	     //$feed->set_cache_location("tmp/simplepie"); 
+	     $feed->set_feed_url("http://www.NuptseFoundation.blogspot.com/feeds/posts/default");
+	     $feed->init();
+             $feed->handle_content_type();
+             $i = 0;
+             foreach($feed->get_items() as $item) {
+	?>
+	     <?php
+		 $i++;
+		 if($i <= 5)
+		 {
+		 ?>
+	        <div class="item">
+	   <h4><?php echo $item->get_title(); ?></h4>
+	    <h5><?php echo $item->get_date('j F Y | g:i a'); ?></h5> 
+		
+	    <?php
+		   if(strlen($item->get_content()) < 225 )
+		   {
+		   
+	          echo $item->get_content();
+		   }
+		   else
+		   {
+		      $str = $item->get_content();
+			  $str2 = substr($str,0,255);
+			  echo $str2;
+		   }
+		   
+		    
+	    ?>
+		<hr>
+		<?php
+		}
+		?>
+		
+	  </div>
+	  <?php
+	  
+	     }
+	     ?>
+		 
+		</ul>
+	</div>
+	</div>
+      </div>
+    </div>
           <p><a class="btn" href="#">View details &raquo;</a></p>
        </div>
         <div class="span5">
