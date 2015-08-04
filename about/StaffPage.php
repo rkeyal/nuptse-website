6196dbf8751f102f3eb1a5e226c077a7c1960f87
LaneiKC126 <kathrynlchan@gmail.com>
2015-08-03 22:45:59 -0700

    Updated student profiles
    for those that have submitted (guys write your profiles)
    I'll do the board member profiles tomorrow.
    Also changed the style color of <hr> to blackish.

--- about/StaffPage.php
+++ about/StaffPage.php
@@ -1,18 +1,185 @@
-<!DOCTYPE html>
++<!DOCTYPE html>
 <html lang="en">
 	<?php include ("../header.php"); ?>
 	<body>
 		<?php include("../navbar/navbar.php"); ?>
 		<div class="container" >
-			<h1>Nuptse Student Staff</h1>
+			<h1 style="text-align:center;">Nuptse Student Staff</h1>
 			<br />
-			<!-- Number 1=============================================================== -->
-			<h3><img src="../images/staff/shivank_nayak.jpg" width="100" height="100" alt="" border="0" />Shivank Nayak, Vice President of Recruitment</h3>
-			<p>Shivank Nayak is a junior at Westview High School. He has been coaching middle and elementary schoolers in math and science since his freshman year. He enjoys participating in academic competitions like Quizbowl. Shivank also does Speech and Debate to develop his public speaking skills. He has also worked with elementary schoolers looking to improve their writing skills. In his free time, Shivank reads and writes. He is interested in theatre and in discussing current social issues.</p>
+			
+										<!------------------------------- AWARENESS ------------------------------->
+			<h2 style="margin-bottom:-25px;">Awareness</h2>
 			<hr />
-			<!-- Number 2============================================================== -->
-			<h3><img src="../images/staff/jefferson_chen.jpg" width="100" height="100" alt="" border="0" />Jefferson Chen, Vice President of Safety and Compliance</h3>
-			<p>Jefferson Chen is a junior at Westview High School. He aims to pursue his passion for STEM with a career in organic chemistry. Jefferson has cultivated a commitment to learning that can be observed through his participation in Westview's Mock Trial team, Chemistry Olympiad, and Harvard's CS50 Computer Science course. Outside of his academic pursuits, Jefferson does volunteer work with Catalyst for Success, Nuptse, and Feeding America. His goal, ultimately, is to foster STEM awareness.</p>
+			<table>
+			  <col width="20%">
+			  <col width="80%">
+				<tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Meilynn Shi</h3>
+					 <h4>President of Awareness || Co-founder</h4>
+					 <p style="line-height:30px">Meilynn, a junior at Westview High School, helped found Nuptse to spark greater interest in mathematics among younger students, especially girls, and bridge school gaps. Meilynn has competed in Math Olympiads, MATHCOUNTS, and AMC 8 and 10. She has coached students, helped organize Math Field Day and outreach events, and now leads the Awareness group in promoting the Nuptse vision and guiding efforts to engage and excite students. At Westview, Meilynn is also a peer counselor, a speech and debate team member, and News Editor for The Nexus, a national-award-winning school newspaper. She enjoys reading, teaching violin and piano performance and theory, channeling her inner energy in martial arts, and exploring science research and ethics.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Karen Ni</h3>
+					 <h4>VP of Advertisement || Photographer</h4>
+					 <p style="line-height:30px">Karen Ni, a senior at Westview High School, is a passionate photographer and an active humanitarian. She is a major proponent of finding new ways to implement the innovative roles of art and creativity in STEM education. Karen also promotes random acts of kindness as the Co-President of Random Acts as well as inspiring people of all ages to discover their interests in the sciences and arts whether or not they decide to pursue these subjects as their careers. She finds happiness in helping others and volunteers for fun. Karen is the Vice President of Catalyst for Success Organization’s San Diego Branch and continues to share her passion of the sciences with others.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Amrit Parida</h3>
+					 <h4>VP of Membership</h4>
+					 <p style="line-height:30px">Amrit Parida is a driven and optimistic performer, piano player and student looking to pursue a career in engineering and business. Growing up with a fascination for different sciences and participating in different math/science competitions such as Science Olympiad has allowed him to cultivate a love for math and engineering. Over the years he has also developed a strong interest in performing as well. An avid debater, he has won several awards on the Westview Speech and Debate club, and qualified for the state tournament this year. In addition, he is also Vice President of South Asian Culture Show, an annual performance in which he dances and acts to raise money for charity. When he isn't learning or performing, Amrit is also event planner for PTSA Student club, and is an active member of National Honors Society and Engineering Club. In his free time, he enjoys listening to all types music, playing tennis, and reading.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Shivank Nayak</h3>
+					 <h4>VP of Recruitment</h4>
+					 <p style="line-height:30px">Shivank Nayak is a junior at Westview High School. He has been coaching middle and elementary schoolers in math and science since his freshman year. He enjoys participating in academic competitions like Quizbowl. Shivank also does Speech and Debate to develop his public speaking skills. He has also worked with elementary schoolers looking to improve their writing skills. In his free time, Shivank reads and writes. He is interested in theatre and in discussing current social issues.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Linette Pan</h3>
+					 <h4>VP of Safety and Compliance</h4>
+					 <p style="line-height:30px">Linette Pan attends The Bishop's School in La Jolla and will graduate in 2017. She is the co-president of Bishop's Middle School Math Club, and also the VP of Bishop's Computer Club. Her team won both Technical Difficulty and Appropriateness to Theme at the San Diego Women's Hackathon in 2014. In her spare time Linette enjoys watching Fast and Furious movies, programming, doing ceramics, and memorizing digits of pi. Linette plays varsity basketball for her high school, winning CIF Open Division freshman year, and was the Sports Representative in class council her sophomore year. She also won both Technical Difficulty and Appropriateness to Theme awards at the San Diego Women's Hackathon in 2014. Outside of school, Linette spends time volunteering for Girls Scouts of San Diego, Air and Space Museum, and San Diego Food Bank. She participates in multiple clubs at school including Korean club and Quanta Science Magazine club. Balancing sports and academics has allowed Linette to succeed in managing her time for extracurricular activities. Her long-term goal is to inspire others to pursue their passions while they maintaining success in the classroom.</p>
+				  </td>
+			   </tr>
+			</table>
+			
+										<!------------------------------- COACHING AND CONTEST ------------------------------->
+			<h2 style="margin-bottom:-25px;">Coaching and Contest</h2>
+			<hr />
+			<table>
+			  <col width="20%">
+			  <col width="80%">
+				<tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Tanuj Pankaj</h3>
+					 <h4>President of Coaching and Contest</h4>
+					 <p style="line-height:30px">Tanuj Pankaj is going to be a junior at Westview High School. He is a co-president of Nuptse, a CSF member, a Key Club member, and a soon-to-be APES tutor. He enjoys watching sports with friends and loves talking about sports. After his time at Westview, Tanuj wants to pursue a college with an outstanding STEM program to continue his interests that he has had since he was little. Lastly, he has a strong interest in math as he competed in math competitions and pursues college courses in math.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Anish Sathe</h3>
+					 <h4>President of Coaching and Contest</h4>
+					 <p style="line-height:30px">Anish Sathe, a junior at Westview, has a passion for math and the sciences. He has participated in many competitions such as the Math and Science Olympiads since elementary school and has placed highly in them. Driven by a love for science, he hopes to continue in STEM-related fields in the future. He participates in many activities both in-school and out, such as Quizbowl, Academic League, and Mock Trial, as well as playing the piano and volunteering at Scripps Memorial Hospital in La Jolla. He likes to read, surf the web, and go running in his free time.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Lucy Zhu</h3>
+					 <h4>VP of Elementary Classes</h4>
+					 <p style="line-height:30px">Lucy Zhu is a junior at Westview High School, and she really enjoys working with other people. This includes anything from group projects to tutoring others. Math has always been her best subject, allowing her to help others who are struggling in that subject. Lucy also takes great pleasure in hands-on activities, such as science experiments. In her free time, she often plays tennis or spends time with her family. She also really enjoys going to the beach to skateboard, swim, or play beach volleyball. However, she gets sunburned really easily.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Chaitanya Kore</h3>
+					 <h4>VP of Middle School Operations</h4>
+					 <p style="line-height:30px">A junior at Westview High School, Chaitanya brings a wide variety of experience to his role at Nuptse. After pursuing science-oriented disciplines during three years at Mesa Verde Middle, he has now branched into academic competitions and service organizations. Chaitanya has benefited from STEM enrichment opportunities in his schools and looks to provide similar initiatives to today's youth. When he is not losing competitions to Carmel Valley Middle or Canyon Crest Academy, Chaitanya enjoys visiting Chicago, cooking pasta, and watching hockey, although not necessarily in that order.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Sunny Gupta</h3>
+					 <h4>VP of Logistics</h4>
+					 <p style="line-height:30px">Sunny Gupta is a junior at Westview High School. His favorite subjects include math and science and hopes to get a degree in  finance/ business/ management. A member of the sports community all round, he loves playing sports mainly tennis, but engages in many others constantly like swimming, running, and biking. These activities makes him the sociable, hardworking, and trustworthy person he is today.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Sayan Chatterjee</h3>
+					 <h4>Math Coach</h4>
+					 <p style="line-height:30px">Sayan Chatterjee is a junior at Del Norte High School and a dedicated student who aspires to pursue a career in science. He especially is interested in computer science, as well as physics. Also, he is a soccer enthusiast, likes playing the piano, and enjoys teaching. Sayan currently volunteers at the Scripps Clinic in Rancho Bernardo and is a member of various clubs at his school.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Joshua Samuel</h3>
+					 <h4>Teacher</h4>
+					 <p style="line-height:30px">Joshua Samuel is a junior at Westview High School. He is greatly interesting in mathematics and science and hopes to pursue an engineering career in the future. Joshua is a proud member of a few clubs on campus such as CSF and Interact. He has also been involved in music such as piano since childhood. For the past year Joshua has been involved in many programs that involve teaching and interacting with children and looks forward to making future connections and friendships through Nuptse.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Vinod Krishnamoorthy</h3>
+					 <h4>Teacher</h4>
+					 <p style="line-height:30px">Vinod was born in New Jersey, but now lives in 4S Ranch, San Diego. He currently attends Del Norte High School. Vinod has lots of experience in math competitions, placing 3rd in San Diego and 12th in So. Cal in Mathcounts when he was in 8th grade, and participating in other prestigious high school level competitions such as the American Invitational Mathematics Examination (AIME) and the American Regions Mathematics League (ARML). Besides mathematics, his other interests include music and athletics.</p>
+				  </td>
+			   </tr>
+			</table>
+			
+										<!------------------------------- SUPPORT FUNCTIONS ------------------------------->
+			<h2 style="margin-bottom:-25px;">Coaching and Contest</h2>
+			<hr />
+			<table>
+			  <col width="20%">
+			  <col width="80%">
+				<tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Rahul Keyal</h3>
+					 <h4>President of Support Functions</h4>
+					 <p style="line-height:30px">Rahul Keyal is a junior at Westview High School. He enjoys swimming, competing in academic tournaments, and also helping others, whether it be through community service events, such as his Eagle Scout Project, or coaching younger students for Quizbowl and Nuptse. Rahul has been a part of Nuptse since its inception during the fall of 2013. Outside of Nuptse, Rahul is President of Westview Quizbowl, for which his team recently attended the High School National Championship Tournament in Chicago, and is also part of the RoboSub team San Diego Robotics 101, which he has been with for two years. Rahul has also competed in prestigious academic competitions such as AIME and ACS.</p>
+				  </td>
+			   </tr>
+				<tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Jefferson Chen</h3>
+					 <h4>VP of Safety and Compliance</h4>
+					 <p style="line-height:30px">Jefferson Chen is a junior at Westview High School. He aims to pursue his passion for STEM with a career in organic chemistry. Jefferson has cultivated a commitment to learning that can be observed through his participation in Westview's Mock Trial team, Chemistry Olympiad, and Harvard's CS50 Computer Science course. Outside of his academic pursuits, Jefferson does volunteer work with Catalyst for Success, Nuptse, and Feeding America. His goal, ultimately, is to foster STEM awareness.</p>
+				  </td>
+			   </tr>
+			</table>
 			<br />
 			
 			<!--
