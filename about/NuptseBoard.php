ea9dacc9ea612703662ebf09129f6d8b398a0987
LaneiKC126 <kathrynlchan@gmail.com>
2015-08-05 17:33:04 -0700

    Added board member profiles

--- about/NuptseBoard.php
+++ about/NuptseBoard.php
@@ -4,15 +4,83 @@
 	<body>
 		<?php include("../navbar/navbar.php"); ?>
 		<div class="container" >
-			<h1>Nuptse Board</h1>
+			<h1 style="text-align:center;">Nuptse Board Members</h1>
 			<br />
-			<!-- Number 1=============================================================== -->
-			<p><img src="images/Image_1.png" width="100" height="100" alt="" border="0">TEXTNAME</p>
-			<p>TEXTPAR</p>
-			<hr />
-			<!-- Number 2============================================================== -->
-			<p><img src="images/Image_2.png" width="100" height="100" alt="" border="0">TEXTNAME2</p>
-			<p>TEXTPAR</p>
+			
+			<table>
+			  <col width="20%">
+			  <col width="80%">
+				<tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Shiv Keyal</h3>
+					 <p style="line-height:30px">
+						Shiv has BS in Mechanical Engineering and MS in Industrial and Systems Engineering. He has worked
+						in quality and manufacturing operations for last about 20 years. He has three children, one in
+						college, one in high school and one in elementary school.</p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Renee (Yuhong) Tang</h3>
+					 <p style="line-height:30px">
+						 Renee Tang has B.S. in Chemical Engineering and Ph. D. in Physical Chemistry, and worked for a local 
+						 company that provides scientific software and services to big pharms and bio-techs for more than 10 
+						 years. Retired last year to take care of family, particularly elderly parents. Two children - 8th grader in 
+						 Mesa Verde and a 4th grader in Deer Canyon. Used to live in Park Village area and moved closer to the 
+						 schools two years ago.
+					 </p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Vinay Sathe</h3>
+					 <p style="line-height:30px">
+						Vinay Sathe has lived in Rancho Penasquitos for over 20 years, having been involved in various 
+						activities in the area elementary, middle and high schools as a parent volunteer. Vinay is licensed to 
+						practice law in California and works as a patent attorney in one of the largest 100 law firms in the 
+						United States. Vinay also has a Ph. D. degree in Electrical Engineering.
+					 </p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Catherine Mintz Macias</h3>
+					 <p style="line-height:30px">
+						Catherine Mintz Macias has BA from UCSD Economics and Masters in Cross Cultural Education. She 
+						is a teacher at Torrey Pines High School, San Dieguito Unified High School District for about 13 years 
+						and Social Science Dept Chair. Catherine is a parent of 3 children in Poway District, Canyon 
+						View/Mesa Verde.
+					 </p>
+				  </td>
+			   </tr>
+			   <tr>
+				  <td>
+					 <img src="" width="90%" height="90%" alt="" border="0" />
+				  </td>
+				  <td>
+					 <h3>Rajesh Pankaj</h3>
+					 <p style="line-height:30px">
+						Dr. Rajesh Pankaj is a Senior Vice President of Engineering at Qualcomm Research. In this role, he leads 
+						a broad portfolio of research projects on mobile computing and wireless communications. Prior to Qualcomm, 
+						he was an Assistant Professor at the University of Toronto. He holds a B.Tech. from IIT Kanpur and an S.M. 
+						and a Ph.D. from MIT. He has been a resident of San Diego since 1997.
+					 </p>
+				  </td>
+			   </tr>
+			   
+			</table>
 			
 			<br />
 			<?php include("../footer.php"); ?>
