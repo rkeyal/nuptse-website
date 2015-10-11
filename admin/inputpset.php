2a3aa8350e72f405174b907fa992bb1dcbb3829c
cdr135 <cdr300947@gmail.com>
2015-10-11 14:56:29 -0700

    Formatting
    Cleaned the code up a bit

--- admin/inputpset.php
+++ admin/inputpset.php
@@ -6,55 +6,59 @@
 		<div class="container" style="padding: 50px 0px">
 			<form id="uploadHere" action="uploadpset.php" enctype="multipart/form-data" method="post">
 				<fieldset>
-				<div class="control-group">
-					Questions PDF: <input type="file" id="problemsetpdf" name="problemsetpdf" value="Questions PDF"><br />
-					Solutions PDF: <input type="file" id="anspdf" name="anspdf"><br />
-					Name: <input type="text" name="name" id="name" placeholder="Enter name here"/><br />
-					Group(s): <!--input type="text" name="group" id="group" placeholder="Enter group(s) here"/><br /-->
+					<div class="control-group">
+						Questions PDF: <input type="file" id="problemsetpdf" name="problemsetpdf" value="Questions PDF"><br />
+						Solutions PDF: <input type="file" id="anspdf" name="anspdf"><br />
+						Name: <input type="text" name="name" id="name" placeholder="Enter name here"/><br />
+						Group(s):
+						<?php
+							$mysqli = new mysqli("localhost", "root", "8PaHucre", "nuptse_system");
+							$query = "SELECT id from groups;";
+							$result = $mysqli->query($query) or die(mysql_error());
+							$groups = "
+						<table style='width: 450px;'>
+							<tr>";
+							$i = 0;
+							$base = 9;
+							while ($row = $result->fetch_assoc()){
+								$id = $row['id'];
+								$groups = $groups . "
+							<td><input type='checkbox' name='group" . $id . "' value='" . $id . "'>  " . $id . "</td>";
+								if (++$i % $base == 0) $groups = $groups . "
+							</tr>
+							<tr>";
+							}
+							$groups = $groups . "
+							</tr>
+						</table>\n";
+							echo '<input type="hidden" name="groups" value=' . $i . '>';
+							$result->free();
+							$mysqli->close();
+							echo $groups;
+						?>
+						<br />
+						Expiry Date: <input type="date" name="expiry" id="expiry"/><br />	<!-- HTML5 only. Defaults to text -->
+					</div>
+					Problem set answers:<br />
 					<?php
-						$mysqli = new mysqli("localhost", "root", "8PaHucre", "nuptse_system");
-						$query = "SELECT id from groups;";
-						$result = $mysqli->query($query) or die(mysql_error());
-						$groups = '<table style="width: 450px;"><tr>';
-						$i = 0;
-						$base = 9;
-						while ($row = $result->fetch_assoc()){
-							$id = $row['id'];
-							$groups = $groups . '
-							<td><input type="checkbox" name="group' . $id . '" value="' . $id . '">  ' . $id . '</td>';
-							if (++$i % $base == 0) $groups = $groups . '</tr>
-							<tr>';
-						}
-						$groups = $groups . "</tr></table>";
-						echo '<input type="hidden" name="groups" value=' . $i . '>';
-						$result->free();
-						$mysqli->close();
-						echo $groups;
-					?> <br />
-					Expiry Date: <input type="date" name="expiry" id="expiry"/><br />	<!-- HTML5 only. Defaults to text -->
-				</div>
-				Problem set answers:<br />
-				<?php
 					for ($i = 1; $i <= $_POST['questioncount']; $i++){
 						echo $i . '<input name="question' . $i . '" type="number" style="padding: 0px 5px">';
-						// Should be a number. HTML5 only. Defaults to text
-					echo "	relative tolerance:  ";
-					echo "<select name='error$i'>
-					<option value='0.00001'>0.00</option>
-					<option value='0.01'>0.01</option>
-					<option value='0.05'>0.05</option>
-					<option value='0.1'>0.1</option>
-					</select><br />";
+							// Should be a number. HTML5 only. Defaults to text
+						echo "	relative tolerance:  ";
+						echo "<select name='error$i'>
+						<option value='0.00001'>0.00</option>
+						<option value='0.01'>0.01</option>
+						<option value='0.05'>0.05</option>
+						<option value='0.1'>0.1</option>
+						</select><br />";
 					}
-				?>
-				<input type="hidden" name="questioncount" value="<?php echo $_POST["questioncount"]; ?>">
-				<button type="submit" class="btn btn-primary">Add Problem Set</button>
+					?>
+					<input type="hidden" name="questioncount" value="<?php echo $_POST["questioncount"]; ?>">
+					<button type="submit" class="btn btn-primary">Add Problem Set</button>
 				</fieldset>
 			</form>
 		</div> <!-- /container -->
-		</div> <!-- /container -->
 		<?php include("../footer.php"); ?>
-		</div> <!-- /container -->
 		<!-- Le javascript
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
@@ -62,13 +66,12 @@
 		<script src="../assets/bootstrap/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="http://jzaefferer.github.com/jquery-validation/jquery.validate.js"></script>
 		<script type="text/javascript">
-			$(document).ready(function(){
-				$("#uploadHere").validate({
-					rules:{
-						problemsetpdf:"required",
-						anspdf:"required",
-						name:"required",
-						//group:"required",
+		$(document).ready(function(){
+			$("#uploadHere").validate({
+				rules:{
+					problemsetpdf:"required",
+					anspdf:"required",
+					name:"required",
 						expiry:{
 							required:true,
 							minlength: 10
@@ -78,7 +81,6 @@
 						problemsetpdf:"Please upload a pset file",
 						anspdf:"Please upload a solution file",
 						name:"Please enter in a name for the pset",
-						//group:"Please enter the groups that the pset is assigned to",
 						expiry:{
 							required:"Please enter an expiry date"
 							minlength:"Date must be 10 characters"
@@ -94,7 +96,7 @@
 						$(element).parents('.control-group').addClass('success');
 					}
 				});
-			});
+		});
 		</script>
 	</body>
 </html>
